6dd3fc6396b6289b5ad77a6e5eec3ea27f225b66
engineerlab.de <mail@nomail.org>
2026-02-18 17:07:27 +0100

    Simple System_Off
    System off after 3times blinking then restart with GPIo D0 with switch to GND

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,50 @@
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h>
 
+#define WAKE_PIN D0   // << change Pin Number for waking up from system off
+
+void enterSystemOff() {
+  Serial.println("System off");
+  Serial.flush();
+  delay(100);  
+
+  // Wakeup-Pin as Input with Pullup
+  pinMode(WAKE_PIN, INPUT_PULLUP);
+
+  // nRF52840 registry for Wakeup trough GPIO LOW
+  NRF_GPIO->PIN_CNF[g_ADigitalPinMap[WAKE_PIN]] |= 
+      (GPIO_PIN_CNF_SENSE_Low << GPIO_PIN_CNF_SENSE_Pos);
+
+  delay(10);
+
+  // System OFF aktivieren
+  NRF_POWER->SYSTEMOFF = 1;
+
+  // wartet hier bis Reset
+  while (1);
+}
+
+
 void setup() {
   Serial.begin(115200);
-  delay(2000);   // USB Zeit geben
-  Serial.println("START");
+  delay(2000);   
+  Serial.println("System On");
+
   pinMode(LED_GREEN, OUTPUT);
 }
 
 void loop() {
 
-  digitalWrite(LED_GREEN, HIGH); // LED AUS
-  Serial.println("RUNNING");
-  delay(500);
-    digitalWrite(LED_GREEN, LOW);  // LED AN
-  delay(500);
+
+    // blink 3 times before sleep
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(LED_GREEN, LOW);   
+    delay(1000);
+    Serial.println("System Off");
+    digitalWrite(LED_GREEN, HIGH);  
+    delay(1000);
+  }
+ 
+ enterSystemOff();
 
 }
