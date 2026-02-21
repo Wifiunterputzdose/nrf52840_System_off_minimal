2e51490699c19493496a7fb163f2a190ea1c02f6
engineerlab.de <mail@nomail.org>
2026-02-21 19:22:14 +0100

    Deep Sleep minimal program with different uf2 file generation

--- uf2_post.py
+++ uf2_post.py
@@ -1,17 +1,23 @@
 Import("env")
+import os
 import sys
 
 def generate_uf2(source, target, env):
-    hex_path = env.subst("$BUILD_DIR/${PROGNAME}.hex")
-    uf2_path = env.subst("$BUILD_DIR/${PROGNAME}.uf2")
+    hex_path = env.subst("$BUILD_DIR/firmware.hex")
+    uf2_path = env.subst("$BUILD_DIR/firmware.uf2")
+
+    project_dir = env.subst("$PROJECT_DIR")
+    uf2conv_path = os.path.join(project_dir, "uf2conv.py")
 
     cmd = (
-        f"\"{sys.executable}\" uf2conv.py "
+        f"\"{sys.executable}\" "
+        f"\"{uf2conv_path}\" "
         f"\"{hex_path}\" "
         f"-c -f 0xADA52840 "
         f"-o \"{uf2_path}\""
     )
 
+    print("Generating UF2...")
     return env.Execute(cmd)
 
-env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", generate_uf2)
+env.AddPostAction("$BUILD_DIR/firmware.hex", generate_uf2)
