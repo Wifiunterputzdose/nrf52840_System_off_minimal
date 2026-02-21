cebaee55811ebd3c27ae81e053a9a130a3cc4d78
engineerlab.de <mail@nomail.org>
2026-02-21 12:33:27 +0100

    Put nrf52840 and SX1262 in low current consumption mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,74 @@
-
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h>
+#include <SPI.h>
+#include <RadioLib.h>
 
-#define WAKE_PIN D0   // << change Pin Number for waking up from system off
+#define WAKE_PIN D0
+
+// Wio-SX1262 (LLCC68)
+#define LORA_CS    D4
+#define LORA_DIO1  D1
+#define LORA_RST   D2
+#define LORA_BUSY  D3
+
+
+LLCC68 radio = new Module(LORA_CS, LORA_DIO1, LORA_RST, LORA_BUSY);
+
+// =====================================================
 
 void enterSystemOff() {
-  Serial.println("System off");
-  Serial.flush();
-  delay(100);  
 
-  // Wakeup-Pin as Input with Pullup
-  pinMode(WAKE_PIN, INPUT_PULLUP);
+  // Sleep mode for SX1262
+  radio.standby();
+  delay(5);
+  radio.sleep();
+  delay(5);
 
-  // nRF52840 registry for Wakeup trough GPIO LOW
-  NRF_GPIO->PIN_CNF[g_ADigitalPinMap[WAKE_PIN]] |= 
-      (GPIO_PIN_CNF_SENSE_Low << GPIO_PIN_CNF_SENSE_Pos);
-
-  delay(10);
-
-  // System OFF aktivieren
+  //nrf52840 deep sleep mode
   NRF_POWER->SYSTEMOFF = 1;
-
-  // wartet hier bis Reset
   while (1);
 }
 
+// =====================================================
 
 void setup() {
-  Serial.begin(115200);
-  delay(2000);   
-  Serial.println("System On");
 
+  Serial.begin(115200);
+  delay(2000);
+
+  Serial.println("System On");
   pinMode(LED_GREEN, OUTPUT);
+
+  Serial.println("Before begin()");
+
+  int state = radio.begin(868.0);
+
+  Serial.println("After begin()"); 
+  radio.setOutputPower(12);
+
+  Serial.print("State: ");
+  Serial.println(state);
 }
+
+// =====================================================
 
 void loop() {
 
-
-    // blink 3 times before sleep
+  // 3x blinking
   for (int i = 0; i < 3; i++) {
-    digitalWrite(LED_GREEN, LOW);   
+    digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
-    Serial.println("System Off");
-    digitalWrite(LED_GREEN, HIGH);  
+    digitalWrite(LED_BUILTIN, HIGH);
     delay(1000);
   }
- 
- enterSystemOff();
 
+  // sending through tx for 5s just to increase current comsumption 
+  Serial.println("Start Continuous TX");
+  radio.standby();  // prepare LoRa mode
+  radio.transmitDirect(); // starting continuous wave
+  delay(5000);   
+
+  //Now go to deep sleep
+  Serial.println("Going into Deep Sleep System OFF");
+  enterSystemOff();
 }
